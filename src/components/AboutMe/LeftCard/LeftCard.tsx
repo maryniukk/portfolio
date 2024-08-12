577f41976f79e2f1bedd4c2fd67a6f9f7b849673
Nikita Maryniuk <nikitamaryniuk@Nikitas-MacBook-Pro.local>
2024-08-12 16:41:53 +0200

    public version

--- src/components/AboutMe/LeftCard/LeftCard.tsx
+++ src/components/AboutMe/LeftCard/LeftCard.tsx
@@ -7,7 +7,7 @@ type Props = {};
 export default function LeftCard({}: Props) {
 	return (
 		<div className='flex items-center ml-auto mr-auto justify-center'>
-			<NeonGradientCard className=' min-w-[450px] relative max-w-sm transition-transform duration-300 ease-in-out hover:scale-105'>
+			<NeonGradientCard className=' min-w-[450px] relative max-w-sm transition-transform duration-300 ease-in-out'>
 				<span className='pointer-events-none z-10 h-full whitespace-pre-wrap bg-gradient-to-br from-[#FF8C02] from-35% to-[#FF8C02] bg-clip-text text-center text-2xl font-bold leading-none tracking-tighter text-transparent dark:drop-shadow-[0_5px_5px_rgba(0,0,0,0.8)]'>
 					<Image
 						className='rounded-lg transition-transform duration-300 ease-in-out hover:scale-110'
